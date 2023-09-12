ce11572bcda9f6d03b8f48d01eace2a72cbc8820
mdowns <michaeljdowns99@gmail.com>
2023-09-12 14:04:34 -0600

    Added React Framework to Project

---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import classes from './Footer.module.css'
+
+const Footer = () => {
+
+    return <footer className={classes.header}>
+        I AM A Footer
+    </footer>
+}
+
+export default Footer;
