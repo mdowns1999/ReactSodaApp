df33776298ffea15e54cfb58fd2793a6be2e3d8a
mdowns <michaeljdowns99@gmail.com>
2023-09-13 18:58:27 -0600

    Started Work on Product Page

---
+++ src/components/Sodas/SodaItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SodaItems = (props) => {
+
+}
+
+export default SodaItems;
---
+++ src/components/Sodas/SodaProducts.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SodaProducts = (props) => {
+
+}
+
+export default SodaProducts;
