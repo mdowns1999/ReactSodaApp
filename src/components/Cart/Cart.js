a1899d4661d544a7617b0797815835cdf9e25b4a
mdowns <michaeljdowns99@gmail.com>
2023-09-18 12:56:41 -0600

    Saving before starting on Cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,24 +1,28 @@
 import React from "react";
 import Modal from "../UI/Modal";
-import classes from './Cart.module.css';
+import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
-
-    const cartItems = <ul>
-        <li>
-            Item
-        </li>
+  const cartItems = (
+    <ul>
+      <CartItem/>
     </ul>
-    return <Modal>
-        <div className={classes.cart}>
-        <h1>Shopping CART</h1>
+  );
+  return (
+    <Modal>
+      <div className={classes.cart}>
+        <h1>Your Shopping Cart</h1>
         {cartItems}
         <div className={classes.cartBtns}>
-        <button  className={classes.cartBtn} onClick={props.onHideCart}>Close</button>
-        <button className={classes.cartBtn}>Order</button>
+          <button className={classes.cartBtn} onClick={props.onHideCart}>
+            Close
+          </button>
+          <button className={classes.cartBtn}>Order</button>
         </div>
-        
-    </div></Modal>
-}
+      </div>
+    </Modal>
+  );
+};
 
 export default Cart;
