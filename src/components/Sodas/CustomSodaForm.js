6f244857e1f5e5a9b978a1e0589510305a8f67ad
mdowns <michaeljdowns99@gmail.com>
2023-09-28 13:47:32 -0600

    Got Check Boxes working

--- src/components/Sodas/CustomSodaForm.js
+++ src/components/Sodas/CustomSodaForm.js
@@ -1,4 +1,32 @@
 import { useState } from "react";
+import classes from './CustomSodaForm.module.css'
+
+// const sodaFlavors = [
+//   {
+//     name: "Pepsi",
+//     price: 1.2
+//   },
+//   {
+//     name: "Sprite",
+//     price: 2.0
+//   },
+//   {
+//     name: "Dr.Pepper",
+//     price: 2.5
+//   },
+//   {
+//     name: "Coke",
+//     price: 3.0
+//   },
+//   {
+//     name: "Rootbeer",
+//     price: 3.5
+//   },
+//   {
+//     name: "Fanta",
+//     price: 3.0
+//   }
+// ];
 
 const sodaSyrups = [
     {
@@ -14,7 +42,7 @@ const sodaSyrups = [
       price: 2.5
     },
     {
-      name: "Onions",
+      name: "Raspberry",
       price: 3.0
     },
     {
@@ -53,12 +81,35 @@ const CustomSodaForm = () => {
         }, 0)
         setPrice(totalPrice);
     }
-    return <form>
+    return <form className={classes.customForm}>
+      {/* <div>
+      <h2>Soda Flavors:</h2>
+    <ul>
+        {sodaFlavors.map(({name, price}, index) => (
+            <li key={index}>
+                <div className={classes.checkBoxBtn}>
+                    <input
+                    className={classes.hidden}
+                    type="checkbox"
+                    id={`custom-check-box-s${index}`}
+                    name={name}
+                    value={name}
+                    onChange={() => handleOnChange(index)}/>
+                    <label htmlFor={`custom-check-box-s${index}`}>{name}</label>
+                </div>
+            </li>
+        ))}
+    </ul>
+      </div> */}
+
+      <div>
+      <h2>Syrup Flavors:</h2>
     <ul>
         {sodaSyrups.map(({name, price}, index) => (
             <li key={index}>
-                <div>
+                <div className={classes.checkBoxBtn}>
                     <input
+                    className={classes.hidden}
                     type="checkbox"
                     id={`custom-check-box-${index}`}
                     name={name}
@@ -69,7 +120,7 @@ const CustomSodaForm = () => {
             </li>
         ))}
     </ul>
-
+      </div>
     <p>Total Price: {totalCost}</p>
 
 
---
+++ src/components/UI/CheckBox.js
@@ -0,0 +1,16 @@
+// const CheckBox = (props) => {
+//     return <>{props.list.map(({name, price}, index) => (
+//         <li key={index}>
+//             <div className={classes.checkBoxBtn}>
+//                 <input
+//                 className={classes.hidden}
+//                 type="checkbox"
+//                 id={`custom-check-box-${index}`}
+//                 name={name}
+//                 value={name}
+//                 onChange={() => handleOnChange(index)}/>
+//                 <label htmlFor={`custom-check-box-${index}`}>{name}</label>
+//             </div>
+//         </li>
+//     ))}</>
+// }
