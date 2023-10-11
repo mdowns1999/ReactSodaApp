b2932619aee382f972bdfc12e2eb9c0d3df87785
mdowns <michaeljdowns99@gmail.com>
2023-10-11 13:51:33 -0600

    Formatted Mavigation file

--- src/components/Layout/Navigation.js
+++ src/components/Layout/Navigation.js
@@ -6,10 +6,25 @@ const Navigation = () => {
     <nav className={classes.navigation}>
       <ul className={classes.list}>
         <li>
-          <NavLink to="/" className={({ isActive }) => (isActive ? classes.active : undefined)} end>Home</NavLink>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? classes.active : undefined
+            }
+            end
+          >
+            Home
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/products" className={({ isActive }) => (isActive ? classes.active : undefined)}>Products</NavLink>
+          <NavLink
+            to="/products"
+            className={({ isActive }) =>
+              isActive ? classes.active : undefined
+            }
+          >
+            Products
+          </NavLink>
         </li>
       </ul>
     </nav>
