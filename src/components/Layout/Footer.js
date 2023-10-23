4d8ebd2a2928b3a22802f80fee0f18d57f0de30c
mdowns <michaeljdowns99@gmail.com>
2023-10-23 16:06:27 -0600

    Worked on basic CSS

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
     const year = new Date().getFullYear();
 
     return <footer className={classes.footer}>
-        <div><p>Contact Us:</p><p>poppinsoda@gmail.com</p> <p>(555)555-5555</p></div>
+        <div><h2>Contact Us:</h2><p>poppinsoda@gmail.com</p> <p>(555)555-5555</p></div>
         <div>&copy; Poppin' {year}</div>
         <div><a href="https://www.instagram.com/" rel="noreferrer" target="_blank"><img className={classes.instagram}
                 src={require('../../images/socialIcons/Instagram_Glyph_Black.png')} alt="Instagram Logo"></img></a>
